2bd76430683e2b9e240ef2ca69fd81a93dc3b62c
Shyam Gorai <spg.144@gmail.com>
2019-12-08 11:42:35 +0530

    salesorder-service created

---
+++ src/main/java/com/salesorder/microservice/salesorderservice/SalesorderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.salesorder.microservice.salesorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesorderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesorderServiceApplication.class, args);
+	}
+
+}
